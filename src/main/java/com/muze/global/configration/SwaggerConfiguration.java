8d61857e3d44c3bbc34788bc84b51863a371dcc7
JeonTaeHyeon <jth660084@gmail.com>
2023-10-18 13:29:55 +0900

    Feat : Init Swagger

---
+++ src/main/java/com/muze/global/configration/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package com.muze.global.configration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Muze",
+                description = "API Statement for Muze",
+                version = "v1"
+        )
+)
+
+public class SwaggerConfiguration {
+    @Bean
+    public GroupedOpenApi like(){
+        String[] paths = {"/v1/like"};
+
+        return GroupedOpenApi
+                .builder()
+                .group("Muze Swagger v1-like")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
