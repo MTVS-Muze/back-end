e7134f225b5479e93d939515b2b06f4263189db1
Jinyongmin <jym7546@gmail.com>
2023-10-19 16:03:07 +0900

    [closes #16] Map domain 커멘드 기능 구현

--- src/main/java/com/muze/domain/map/command/domain/aggregate/entity/Map.java
+++ src/main/java/com/muze/domain/map/command/domain/aggregate/entity/Map.java
@@ -1,10 +1,15 @@
 package com.muze.domain.map.command.domain.aggregate.entity;
 
 import com.muze.domain.map.command.domain.aggregate.vo.MemberVO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity
+@Getter
+@Entity(name = "Map")
+@Table(name = "MAP_TB")
+@NoArgsConstructor
 public class Map {
 
     @Id
@@ -16,4 +21,13 @@ public class Map {
 
     @Column(columnDefinition = "LONGTEXT")
     private String data;
+
+    public Map(MemberVO memberId, String data) {
+        this.memberId = memberId;
+        this.data = data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
