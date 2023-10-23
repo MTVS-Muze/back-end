5c2ef51b34de02cff0f6503841acbbdbb13eebff
JeonTaeHyeon <jth660084@gmail.com>
2023-10-23 10:52:54 +0900

    [Closes #23]Feat : MapInPlaylist entity 생성

--- src/main/java/com/muze/domain/playlist/command/domain/aggregate/entity/Playlist.java
+++ src/main/java/com/muze/domain/playlist/command/domain/aggregate/entity/Playlist.java
@@ -3,9 +3,11 @@ package com.muze.domain.playlist.command.domain.aggregate.entity;
 import com.muze.domain.playlist.command.domain.aggregate.vo.MemberVO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "PLAYLIST_TB")
@@ -21,9 +23,14 @@ public class Playlist {
 
     @Column
     private String title;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private LocalDateTime createdDate;
     public Playlist(MemberVO memberId, String title) {
         this.memberId = memberId;
         this.title = title;
+        this.createdDate = LocalDateTime.now();
     }
 }
 
